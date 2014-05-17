a9cda8e753ce6acae3e8335d0180cd0938d1664a
iso13 <isorunner@yahoo.com>
2014-05-16 23:38:26 -0400

    Initial commit

---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,64 @@
+<?php
+
+use Behat\Behat\Context\ClosuredContextInterface,
+    Behat\Behat\Context\TranslatedContextInterface,
+    Behat\Behat\Context\BehatContext,
+    Behat\Behat\Exception\PendingException,
+    Behat\MinkExtension\Context\MinkContext;
+use Behat\Gherkin\Node\PyStringNode,
+    Behat\Gherkin\Node\TableNode;
+
+//
+// Require 3rd-party libraries here:
+//
+//   require_once 'PHPUnit/Autoload.php';
+//   require_once 'PHPUnit/Framework/Assert/Functions.php';
+//
+
+/**
+ * Features context.
+ */
+class FeatureContext extends MinkContext
+{
+    /**
+     * Initializes context.
+     * Every scenario gets its own context object.
+     *
+     * @param array $parameters context parameters (set them up through behat.yml)
+     */
+    public function __construct(array $parameters)
+    {
+        // Initialize your context here
+    }
+
+    /** @BeforeScenario */
+    public function setupSelenium()
+    {
+
+        $this->getSession()->getDriver()->resizeWindow(1620.0, 1028.0, 'current');
+
+    }
+
+
+    /**
+     * @Given /^wait (\d+) seconds$/
+     */
+    public function waitSeconds($seconds)
+    {
+        $this->getSession()->wait(1000*$seconds);
+    }
+
+
+
+//
+// Place your definition and hook methods here:
+//
+//    /**
+//     * @Given /^I have done something with "([^"]*)"$/
+//     */
+//    public function iHaveDoneSomethingWith($argument)
+//    {
+//        doSomethingWith($argument);
+//    }
+//
+}
